e9ca594aa4341db9138a4ab3eb307539a57fd5c9
XuXuebin <ddsbn@126.com>
2021-12-23 11:31:08 +0800

    Update model.go

--- src/ngrok/client/model.go
+++ src/ngrok/client/model.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	defaultServerAddr   = "tunnel.smartcellphone.cn:32784"
+	defaultServerAddr   = "tunnel.smartcellphone.cn:4443"
 	defaultInspectAddr  = "127.0.0.1:4040"
 	pingInterval        = 20 * time.Second
 	maxPongLatency      = 15 * time.Second
